b653558aabdb745c4befc6b2af5597c4033976f1
vintej <vtejankar@criterionnetworks.com>
2017-07-07 14:13:06 +0530

    Update params.pp
    Updating mitaka repo

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,14 +3,14 @@
 class tacker::params {
   #include ::openstacklib::defaults
 
-  $client_package_name = 'git+https://github.com/openstack/python-tackerclient.git@stable/mitaka'
+  $client_package_name = 'git+https://github.com/openstack/python-tackerclient.git@mitaka-eol'
   case $::osfamily {
     'RedHat': {
-      $package_name     = 'git+https://github.com/openstack/tacker@stable/mitaka'
+      $package_name     = 'git+https://github.com/openstack/tacker@mitaka-eol'
       $service_name     = 'openstack-tacker-server'
     }
     'Debian': {
-      $package_name     = 'git+https://github.com/openstack/tacker.git@stable/mitaka'
+      $package_name     = 'git+https://github.com/openstack/tacker.git@mitaka-eol'
       $service_name     = 'tacker-server'
       $sqlite_package_name  = 'python-pysqlite2'
       $pymysql_package_name = 'python-pymysql'
